6ced1f0239196ae5ca79db9403b6696f2cb1cab8
Typhanie <typhanie.breteche@outlook.fr>
2025-11-27 15:32:16 +0100

    Modification des données côté serveur phonebook

--- part1/phonebook/src/App.jsx
+++ part1/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
-import axios from 'axios'
+import personsService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -10,11 +10,11 @@ const App = () => {
   const [newPhone, setNewPhone] = useState('')
   const [searchPerson, setSearchPerson] = useState('')
 
-    useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -26,33 +26,81 @@ const App = () => {
       return
     }
 
-    const nameExists = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
-    if (nameExists) {
-      alert(`${newName} est déjà dans le répertoire !`)
-      return
+    const existingPerson = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+   
+    const personObject = { name: newName, number: newPhone };
+
+    if (existingPerson) {
+      if (existingPerson.number === newPhone) {
+        alert(`${newName} est déjà dans le répertoire !`);
+        return;
+      }
+      const confirmUpdate = window.confirm(
+        `${newName} existe déjà. Voulez-vous remplacer l'ancien numéro par celui-ci ?`
+      );  
+      if (confirmUpdate) {
+          handleUpdate(existingPerson.id, personObject);
+          resetForm();
+      }
+      return;
     }
 
-    const personbject = {
-      name: newName,
-      phone: newPhone
-    }
-
-    setPersons(persons.concat(personbject))
-    setNewName('')
-    setNewPhone('')
+    personsService
+      .create(personObject)
+      .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson));
+          resetForm();
+      })
   }
+
+
+  const resetForm = () => {
+    setNewName('');
+    setNewPhone('');
+  };
+
   const handleAddPerson = (event) => {
     setNewName(event.target.value)
-  }
+  };
+
   const handleAddPhone = (event) => {
     setNewPhone(event.target.value)
-  }
+  };
+
   const handleSearchPerson = (event) => {
     setSearchPerson(event.target.value)
-  }
+  };
+
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchPerson.toLowerCase())
-  )
+  );
+
+  const handleDelete = (id) => {
+    personsService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id));
+      })
+      .catch(error => {
+        console.error("Erreur lors de la suppression :", error);
+        alert("Impossible de supprimer la personne !");
+      });
+
+  };
+
+  const handleUpdate = (id, personbject) => {
+    personsService
+      .update(id, personbject)
+      .then(returnedPerson => {
+        setPersons(persons.map(person =>
+          person.id === id ? returnedPerson : person
+        ));
+      })
+      .catch(e =>console.error('Erreur lors de la mise à jour :', e))
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -66,7 +114,7 @@ const App = () => {
         handleAddPhone={handleAddPhone}
       />
       <h3>Numbers</h3>
-      <Persons filteredPersons={filteredPersons} />
+      <Persons filteredPersons={filteredPersons} handleDelete={handleDelete} />
     </div>
   )
 }
--- part1/phonebook/src/Persons.jsx
+++ part1/phonebook/src/Persons.jsx
@@ -1,10 +1,11 @@
 
-const Persons = ({ filteredPersons }) => {
+const Persons = ({ filteredPersons, handleDelete }) => {
   return (
       <ul>
         {filteredPersons.map(person =>
           <li key={person.name}>
-            {person.name} {person.phone}
+            {person.name} {person.number}
+            <button onClick={() => handleDelete(person.id)}>Delete</button>
           </li>
         )}
       </ul>
